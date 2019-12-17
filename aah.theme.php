56c5a13ab7287584c0c1415a463a4482baeb9e29
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2019-12-17 15:53:01 +0000

    initial commit, compiling ok

---
+++ aah.theme.php
@@ -0,0 +1,17 @@
+<?php
+// This file declares a CSS theme for CiviCRM.
+
+return array (
+  'name' => 'aah',
+  'title' => 'aah theme',
+  'prefix' => 'aah/',
+  'url_callback' => '\\Civi\\Core\\Themes\\Resolvers::simple',
+  'search_order' => 
+  array (
+    0 => 'aah',
+    1 => '_fallback_',
+  ),
+  'excludes' => 
+  array (
+  ),
+);
